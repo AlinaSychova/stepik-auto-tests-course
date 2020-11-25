31a2bb0e4f45777a9c1261cce9a7143adb632d88
Alina Sychova <alinasychova@Alisas-MacBook-Pro.local>
2020-11-25 13:20:58 +0200

    new files

--- lesson6_step10.py
+++ lesson6_step10.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
-import time 
-from selenium.webdriver.common.by import By
+import time
 
-try: 
+try:
     link = "http://suninjuly.github.io/registration2.html"
     browser = webdriver.Chrome()
     browser.get(link)
